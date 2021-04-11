bb6ce5ac9050eb00ec5c1c68f25978dd4073ad25
Oluwatunmise <oluwatunmiseadenuga@crenettechlabs.com>
2021-04-12 00:11:24 +0100

    chore(action): created photo action

---
+++ app/src/store/action/photos.js
@@ -0,0 +1,34 @@
+import { GET_PHOTOS_ERROR, GET_PHOTOS, SET_PHOTOS } from './../actionTypes/photos';
+import { searchPhotos } from './../../services/search';
+
+const searchRequest = (_) => {
+  return {
+    type: GET_PHOTOS
+  }
+}
+
+const searchRequestSuccessResponse = (payload) => {
+  return {
+    type: SET_PHOTOS,
+    payload
+  }
+}
+
+const searchRequestErrorResponse = (payload) => {
+  return {
+    type: GET_PHOTOS_ERROR,
+    payload
+  }
+}
+
+export const getPhotosAsync = async (dispatch, query) => {
+  dispatch(searchRequest)
+  try {
+    dispatch(GET_PHOTOS)
+    const apiResponse = searchPhotos(query)
+    dispatch(searchRequestSuccessResponse(apiResponse))
+  } catch (getPhotosAsyncError) {
+    dispatch(searchRequestErrorResponse(error.response.data.message) || 'Something went wrong')
+    throw new Error(error.response.data.message || 'something went wrong')
+  }
+}
